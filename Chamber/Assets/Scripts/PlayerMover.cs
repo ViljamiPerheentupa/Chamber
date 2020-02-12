9c06c901bcb27a36c17c571d638bff576106266f
Viljami Perheentupa <pviljo@gmail.com>
2020-02-12 14:49:10 +0200

    Liikkumista

--- Chamber/Assets/Scripts/PlayerMover.cs
+++ Chamber/Assets/Scripts/PlayerMover.cs
@@ -5,18 +5,51 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour {
     Rigidbody rig;
     public float movementSpeed;
-    public float maxVelocityChange;
+    //public float maxVelocityChange;
+    float vertical;
+    float horizontal;
+    float momentum = 0;
+    public float momentumMultiplier = 10;
+    float maxMomentum = 1;
+    public float maxVelocity = 2;
+    Vector3 lastPosition = Vector3.zero;
+    Vector3 positionChange = Vector3.zero;
     void Start() {
         rig = GetComponent<Rigidbody>();
     }
 
     void Update() {
-        float vertical = Input.GetAxisRaw("Vertical");
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        print("Horizontal: " + )
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        print(momentum);
     }
 
-    private void LateUpdate() {
+    private void FixedUpdate() {
+        Vector3 inputVector = new Vector3(horizontal, 0, vertical);
+        //inputVector = transform.TransformDirection(inputVector);
+        if (inputVector.magnitude > 0 && momentum < maxMomentum) {
+            momentum += Time.deltaTime * momentumMultiplier;
+            if (momentum > maxMomentum) {
+                momentum = maxMomentum;
+            }
+        }
+        if (HasInput() && HasMomentum()) {
+            momentum -= Time.deltaTime * momentumMultiplier;
+            if (momentum < 0) {
+                momentum = 0;
+            }
+        }
+        Vector3 velocity = inputVector * movementSpeed * momentum;
+        //velocity = Vector3.ClampMagnitude(velocity, maxVelocity);
+        if (HasMomentum()) {
+            lastPosition = rig.position;
+            rig.position += velocity * Time.deltaTime;
+            positionChange = rig.position - lastPosition;
+        }
+        if (HasMomentum() && !HasInput()) {
+            rig.position += positionChange * movementSpeed * momentum;
+        }
+
         //Vector3 inputVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         //inputVelocity = transform.TransformDirection(inputVelocity);
         //Vector3 targetVelocity = inputVelocity * movementSpeed;
@@ -27,5 +60,17 @@ public class PlayerMover : MonoBehaviour {
         //velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
         //velocityChange.y = 0;
         //rig.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+
+    bool HasInput() {
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Vertical") > 0) {
+            return true;
+        } else return false;
+    }
+
+    bool HasMomentum() {
+        if (momentum > 0) {
+            return true;
+        } else return false;
     }
 }
