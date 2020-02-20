14dd132dfa4e2d051fca47230d2ec83c657b3109
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-02-20 13:31:22 +0200

    Enemy AI + Navmesh + Moving Obstacles

--- Chamber/Assets/Scripts/EnemyAI.cs
+++ Chamber/Assets/Scripts/EnemyAI.cs
@@ -2,43 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class EnemyAI : MonoBehaviour
 {
-    public enum Enemystate { Idle, Hunting, Hiding, Shooting };
+    public enum Enemystate { Idle, Hunt, Shoot, Hide, Sneak };
 
     public Enemystate es;
+    bool huntingStarted;
     GameObject player;
     Vector3 target;
     NavMeshAgent agent;
     float detectionDistance = 5f;
     float targetProximityDistance = 2f;
-
+    UnityAction[] actions;
+    
     void Start() {
         es = Enemystate.Idle;
         player = GameObject.Find("PlayerBody");
         agent = GetComponent<NavMeshAgent>();
+        actions = new UnityAction[] { Idle, Hunt, Shoot, Hide, Sneak };
     }
 
-    void StartHunting() {
-        target = player.transform.position;
-        agent.destination = target;
-        es = Enemystate.Hunting;
-    }
-
-    void StartIdling() {
-        es = Enemystate.Idle;
-        agent.destination = transform.position;
-    }
-
-    void Update() {
-        if(Vector3.Distance(agent.transform.position, player.transform.position ) < detectionDistance) {
-            StartHunting();
+    #region Actions
+    void Idle() {
+        if(Vector3.Distance(agent.transform.position, player.transform.position) < detectionDistance) {
+            es = Enemystate.Hunt;
         }
-        if (es == Enemystate.Hunting) {
-            if(Vector3.Distance(agent.transform.position, target) < targetProximityDistance) {
-                StartIdling();
-            }
+    }
+    void Hunt() {
+        if(!huntingStarted) {
+            target = player.transform.position;
+            agent.destination = target;
         }
+        if(Vector3.Distance(agent.transform.position, target) < targetProximityDistance) {
+            es = Enemystate.Idle;
+            agent.destination = transform.position;
+            huntingStarted = false;
+        }
+    }
+    void Shoot() {
+    
+    }
+    void Hide() {
+    
+    }
+    void Sneak() {
+    
+    }
+    #endregion
+
+    private void Update() {
+        actions[(int)es]();
     }
 }
