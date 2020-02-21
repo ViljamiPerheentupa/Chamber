0200cc928d892e7140ad8b759bd6c2f3f981eff5
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-02-21 14:23:20 +0200

    AI Refinement

--- Chamber/Assets/Scripts/EnemyAI.cs
+++ Chamber/Assets/Scripts/EnemyAI.cs
@@ -5,114 +5,146 @@ using UnityEngine.AI;
 using UnityEngine.Events;
 
 public class EnemyAI : MonoBehaviour
-{
-    public enum Enemystate { Idle, Alert, Hunt, Shoot, Hide, Sneak }; // Todo: Alert?
+{ 
+    public enum Enemystate { Idle, Alert, Hunt, Shoot, Hide, Sneak, Stunned };
 
     public Enemystate es;
-    
-    bool idling;
-    bool alerted;
-    bool hunting;
-    bool shooting;
-    bool hiding;
-    bool sneaking;
+    Enemystate memES;
+    Enemystate previousES;
+
+    float[] detectionDistances = new float[] { 25f, 35f, 35f, 35f, 35f, 3543/7, 0 };
 
     GameObject player;
     Vector3 target;
     NavMeshAgent agent;
+    LayerMask layerMask;
 
-    float idleDetectionDistance = 15f;
-    float alertDetectionDistance = 25f;
-    float shootingDistance = 10f;
-    float targetProximityDistance = 5f;
+    float viewAngle = 90f;
+    float alertDistance = .5f;
 
-    float alertTimer = 5f;
+    float targetProximityDistance = .5f;
+
+    float timer;
+    float alertTime = 5f;
+    float huntTime = 2.5f; 
 
     UnityAction[] actions;
-    
+
     void Start() {
         es = Enemystate.Idle;
         player = GameObject.Find("PlayerBody");
         agent = GetComponent<NavMeshAgent>();
-        actions = new UnityAction[] { Idle, Alert, Hunt, Shoot, Hide, Sneak }; // Todo: Alert?
+        actions = new UnityAction[] { Idle, Alert, Hunt, Shoot, Hide, Sneak, Stunned };
+        layerMask = LayerMask.GetMask(new string[] { "Environment" });
     }
 
     #region Actions
     void Idle() {
-        if(!idling) {
-            idling = true;
+        if(previousES != Enemystate.Idle) {
             print("Idling");
         }
-        if(Vector3.Distance(agent.transform.position, player.transform.position) < idleDetectionDistance) {
+
+        if(PlayerSeen()) {
             es = Enemystate.Hunt;
-            idling = false;
+            return;
+        }
+
+        if(PlayerHeard()) {
+            es = Enemystate.Alert;
         }
     }
     void Alert() {
-        if(!alerted) {
-            alertTimer = Time.time + 5f;
-            alerted = true;
+        if(previousES != Enemystate.Alert) {
+            timer = Time.time + alertTime;
             print("Alerted");
+            var pos = transform.position;
+            target = (Vector3.Normalize(player.transform.position - pos) * alertDistance) + transform.position;
+            agent.destination = target;
         }
 
-        if(Time.time > alertTimer) {
-            es = Enemystate.Idle;
-            alerted = false;
-            // Todo: Return to original post maybe?
-        }
-
-        if(Vector3.Distance(agent.transform.position, player.transform.position) < alertDetectionDistance) {
+        if(PlayerSeen()) {
             es = Enemystate.Hunt;
-            alerted = false;
+            return;
         }
 
-        // Todo: Small area patrol
+        if(Time.time > timer) {
+            es = Enemystate.Idle;
+        }
     }
     void Hunt() {
-        if(!hunting) {
+        if(previousES != Enemystate.Hunt) {
+            timer = Time.time + huntTime; ;
+            print("Hunting");
+        }
+        if(PlayerSeen()) {
             target = player.transform.position;
             agent.destination = target;
-            hunting = true;
-            print("Hunting");
-        } //Todo: think logic below
-        if(Vector3.Distance(agent.transform.position, target) < targetProximityDistance) {
-            if(Vector3.Distance(agent.transform.position, player.transform.position) > alertDetectionDistance) {
-                es = Enemystate.Alert;
-                agent.destination = transform.position;
-                hunting = false;
-            } else if (Vector3.Distance(agent.transform.position, player.transform.position) > shootingDistance){
-                es = Enemystate.Shoot;
-                hunting = false;
-            } else {
-                target = player.transform.position;
-                agent.destination = target;
-            }
+            return;
+        } else if(PlayerHeard()) {
+            es = Enemystate.Alert;
+            return;
+        }
+        if(Time.time > timer) {
+            es = Enemystate.Alert;
         }
     }
+
     void Shoot() {
-        if(!shooting) {
-            shooting = true;
+        if(previousES != Enemystate.Shoot) {
+            // Shoot
             print("Shooting");
         } else {
             es = Enemystate.Alert;
-            shooting = false;
         }
     }
     void Hide() {
-        if(!hiding) {
-            hiding = true;
+        if(previousES != Enemystate.Hide) {
             print("Hiding");
         }
     }
     void Sneak() {
-        if(!sneaking) {
-            sneaking = true;
-            print("sneaking");
+        if(previousES != Enemystate.Sneak) {
+            print("Sneaking");
+        }
+    }
+    void Stunned() {
+        if(previousES != Enemystate.Stunned) {
+            timer = Time.time + .5f;
+            print("Stunned");
+        }
+        if(Time.time > timer) {
+            es = Enemystate.Alert;
         }
     }
     #endregion
 
+    #region Senses
+
+    bool PlayerHeard() {
+        var d = Vector3.Distance(agent.transform.position, player.transform.position);
+        return d < detectionDistances[(int)es];
+    }
+
+    bool PlayerSeen() {
+        // angle of transform forward to player and raycast
+        var pPos = player.transform.position;
+        var tPos = transform.position;
+        var delta = pPos - tPos;
+        var len = delta.magnitude;
+        return len < detectionDistances[(int)es] && 
+            Vector3.Angle(transform.forward, delta) < viewAngle / 2 &&
+            !Physics.Raycast(tPos, delta, len, layerMask);
+    }
+
+    bool TookHit() {
+        return false;
+    }
+
+    #endregion
+
     private void Update() {
+        memES = es;
         actions[(int)es]();
+        previousES = memES;
     }
 }
