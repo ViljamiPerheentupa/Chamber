03c51609f15776f5923a70dc087aaaf99a81b0df
Karim Abdel Hamid <karimah@aucegypt.edu>
2020-05-05 19:00:02 +0200

    Crouching

--- Chamber/Assets/Scripts/NewScripts/PlayerMoverNew.cs
+++ Chamber/Assets/Scripts/NewScripts/PlayerMoverNew.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class PlayerMoverNew : MonoBehaviour {
-    public float gravity = 9.81f;
     public float airSpeed = 2.0f;
     public float runSpeed = 3.0f;
     public float sprintSpeed = 8.0f;
@@ -16,6 +15,15 @@ public class PlayerMoverNew : MonoBehaviour {
     public float maintainAirSpeedCoefficient = 0.98f;
     public float maintainSpeedCoefficient = 0.8f;
     public LayerMask floorMask = ~(1 << 9); 
+    public float cameraHeight = -0.25f;
+    public float standCapsuleHeight = 2.0f;
+    public float crouchCapsuleHeight = 1.0f;
+    public float startCrouchTime = 0.0f;
+    public float crouchDelay = 0.5f;
+    public bool isCrouching = false;
+    public AnimationCurve crouchCurve;
+    public Transform cameraTransform;
+
 
     private float endOfJumpTime = 0.0f;
 
@@ -30,6 +38,12 @@ public class PlayerMoverNew : MonoBehaviour {
     bool isGrounded() {
         return Physics.CheckCapsule(c_collider.bounds.center,new Vector3(c_collider.bounds.center.x,c_collider.bounds.min.y-0.1f,c_collider.bounds.center.z),0.18f, floorMask);
     }
+
+    bool CanStand() {
+        Vector3 topOfHead = transform.position;
+        float distance = 1.0f;
+        return !Physics.Raycast(topOfHead, Vector3.up, distance, floorMask); 
+    }
     
     void Update() {
         if (Input.GetButtonDown("Jump")) {
@@ -38,9 +52,54 @@ public class PlayerMoverNew : MonoBehaviour {
                 endOfJumpTime = Time.time + jumpContinuationTime;
             }
         }
+        
+        float t = crouchDelay - (Time.time - startCrouchTime);
+        t = Mathf.Clamp(t, 0.0f, crouchDelay);
+        if (Input.GetKey("c")) {
+            if (!isCrouching) {
+                isCrouching = true;
+                startCrouchTime = Time.time - t;
+            }
+        }
+        else {
+            if (isCrouching) {
+                if (CanStand()) {
+                    isCrouching = false;
+                    startCrouchTime = Time.time - t;
+                }
+                else {
+                    Debug.Log("Can't stand");
+                }
+            }
+        }
+    }
+
+    void HandleCrouch() {
+        /*cameraHeight = -0.25f;
+        public float standCapsuleHeight = 2.0f;
+        public float crouchCapsuleHeight = 1.0f;
+        public float startCrouchTime = 0.0f;
+        public float crouchDelay;
+
+        float currentCamHeight*/
+
+        float t = (Time.time - startCrouchTime) / crouchDelay;
+        if (isCrouching) t = 1 - t;
+
+        t = crouchCurve.Evaluate(t);
+
+        float currentCapsuleHeight = Mathf.Lerp(crouchCapsuleHeight, standCapsuleHeight, t);
+        float currentCapsulePosition = (currentCapsuleHeight - standCapsuleHeight) / 2.0f;
+        float currentCameraHeight = currentCapsulePosition + currentCapsuleHeight/2.0f + cameraHeight;
+        CapsuleCollider capsuleCollider = GetComponent<CapsuleCollider>();
+        capsuleCollider.height = currentCapsuleHeight;
+        capsuleCollider.center = new Vector3(0.0f, currentCapsulePosition, 0.0f);
+        cameraTransform.localPosition = new Vector3(0.0f, currentCameraHeight, 0.0f);
     }
 
     void FixedUpdate() {
+        HandleCrouch();
+
         bool isSprinting = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
         float moveSpeed = 0.0f;
 
