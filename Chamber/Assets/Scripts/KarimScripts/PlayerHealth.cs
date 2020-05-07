db1d10a1fe0143a3882bb4a492fe558f47a360f5
Karim Abdel Hamid <karimah@aucegypt.edu>
2020-05-07 19:26:10 +0200

    Initial work on turret, health for player + props

---
+++ Chamber/Assets/Scripts/KarimScripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : BaseHealth {
+    public float timeToRecover = 2.0f;
+    public float healthRecoverPerSec = 5.0f;
+    public AnimationCurve recoverAnimation = AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f);
+    
+    private float startHealTime = 0.0f;
+
+    public override void TakeDamage(float dmg) {
+        base.TakeDamage(dmg);
+        Debug.Log("Damage: " + dmg + ", Health: " + currentHealth);
+        startHealTime = Time.time + timeToRecover;
+    }
+
+    protected override void Die() {
+        Debug.Log("Dead");
+    }
+
+    void Update() {
+        if (Time.time > startHealTime) {
+            currentHealth = Mathf.Min(maximumHealth, currentHealth + healthRecoverPerSec * Time.deltaTime * recoverAnimation.Evaluate(Time.time - startHealTime));
+        }
+    }
+}
