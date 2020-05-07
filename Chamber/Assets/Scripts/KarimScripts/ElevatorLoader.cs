dfba39bf74953c8dc4237a11857914eee67e8803
Karim Abdel Hamid <karimah@aucegypt.edu>
2020-05-07 05:18:06 +0200

    Elevator Loading: Added Oneway, fixed timing

--- Chamber/Assets/Scripts/KarimScripts/ElevatorLoader.cs
+++ Chamber/Assets/Scripts/KarimScripts/ElevatorLoader.cs
@@ -11,41 +11,76 @@ public class ElevatorLoader : MonoBehaviour {
     public DoorSlider doorA;
     public DoorSlider doorB;
     public bool elevatorCleared = true;
+    [Tooltip("The minimum time to wait before opening the door (in case the level loads too fast).")]
+    public float minimumWaitTime = 1.0f;
+    [Tooltip("If checked, you can only go from A to B")]
+    public bool oneWay;
+
+    private SceneManagerUtil sceneManager;
+    private float openDoorTime;
+
+    void Start() {
+        GameObject sceneManagerObj = GameObject.Find("SceneManager");
+        if (sceneManagerObj) {
+            sceneManager = sceneManagerObj.GetComponent<SceneManagerUtil>();
+        }
+    }
 
     public void LeftElevator() {
         elevatorCleared = true;
+    }
+
+    void UnloadSceneA() {
+        if (sceneManager)
+            sceneManager.UnloadScene(sceneNameA);
+    }
+
+    void UnloadSceneB() {
+        if (sceneManager)
+            sceneManager.UnloadScene(sceneNameB);
+    }
+
+    IEnumerator WaitToOpen(AsyncOperation asyncOperation, DoorSlider slider) {
+        while (!asyncOperation.isDone || Time.time < openDoorTime) {
+            yield return null;
+        }
+
+        slider.Open();
     }
 
     private void OnTriggerEnter(Collider other) {
         if (elevatorCleared) {
             elevatorCleared = false;
 
-            GameObject sceneManagerObj = GameObject.Find("SceneManager");
-            if (sceneManagerObj) {
-                SceneManagerUtil sceneManager = sceneManagerObj.GetComponent<SceneManagerUtil>();
-                if (sceneManager) {
-                    if (SceneManager.GetSceneByName(sceneNameB).isLoaded) {
-                        sceneManager.LoadScene(sceneNameA);
-                        doorB.Close();
-
-                        // Do this on loadscene complete
-                        doorA.Open();
-
-                        // Unload scene after door close
-                        sceneManager.UnloadScene(sceneNameB);
-                    }
-                    else {
-                        sceneManager.LoadScene(sceneNameB);
+            if (sceneManager) {
+                if (SceneManager.GetSceneByName(sceneNameA).isLoaded) {
+                    AsyncOperation asyncOperation = sceneManager.LoadScene(sceneNameB);
+                    if (asyncOperation != null) {
+                        openDoorTime = Time.time + minimumWaitTime;
                         doorA.Close();
-                        
+
+                        // Unload scene after door close
+                        Invoke("UnloadSceneA", doorA.timeToMove);
+
                         // Do this on loadscene complete
-                        doorB.Open();
+                        StartCoroutine(WaitToOpen(asyncOperation, doorB));
+                    }
+                }
+                else if (!oneWay) {
+                    openDoorTime = Time.time + minimumWaitTime;
+                    AsyncOperation asyncOperation = sceneManager.LoadScene(sceneNameA);
+                    if (asyncOperation != null) {
+                        doorB.Close();
                         
                         // Unload scene after door close
-                        sceneManager.UnloadScene(sceneNameA);
+                        Invoke("UnloadSceneB", doorB.timeToMove);
+                        
+                        // Do this on loadscene complete
+                        StartCoroutine(WaitToOpen(asyncOperation, doorA));
                     }
                 }
             }
+        
         }
     }
 }
