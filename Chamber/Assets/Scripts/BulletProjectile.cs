7cb2ec8756cfe43d08ab758fa73684038721a7a3
Karim Abdel Hamid <karimah@aucegypt.edu>
2020-05-16 14:34:05 +0200

    Added timelock bullet decal

--- Chamber/Assets/Scripts/BulletProjectile.cs
+++ Chamber/Assets/Scripts/BulletProjectile.cs
@@ -7,17 +7,24 @@ public class BulletProjectile : MonoBehaviour {
     public float moveSpeed = 3.0f;
     public float dmg = 50.0f;
     public LayerMask layerMask = 0xff;
+    public Vector3 decalSize;
+    public Material decalMaterial;
 
     void Start() {
-        Invoke("DestroyMe", 20.0f);
+        Invoke("DestroyMe", 60.0f);
     }
 
     void Update() {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, moveSpeed * Time.deltaTime, layerMask)) {
+        if (Physics.Raycast(transform.position, -transform.forward, out hit, moveSpeed * Time.deltaTime, layerMask)) {
             BaseHealth health = hit.collider.GetComponent<BaseHealth>();
             if (health) {
                 health.TakeDamage(dmg, source);
+            }
+
+            DecalManager dm = FindObjectOfType<DecalManager>();
+            if (dm) {
+                dm.NewDecal(hit.point, Quaternion.LookRotation(hit.normal), decalSize, decalMaterial, hit.collider.transform);
             }
 
             DestroyMe();
