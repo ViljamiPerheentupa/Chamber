f5ac21f76f962c13aaa5a71b8fd1d349c5be5fb9
Karim Abdel Hamid <karimah@aucegypt.edu>
2020-05-17 03:12:50 +0200

    Stalker Cam

---
+++ Chamber/Assets/Scripts/VideoCameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCameraController : MonoBehaviour {
+    public float rotSpeed = 1f;
+
+    void Update() {
+        Transform tr = Camera.main.transform;
+        Vector3 diff = tr.position - transform.position;
+        Quaternion targetQuat = Quaternion.LookRotation(Vector3.Normalize(diff));;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetQuat, rotSpeed * Time.deltaTime);
+    }
+}
