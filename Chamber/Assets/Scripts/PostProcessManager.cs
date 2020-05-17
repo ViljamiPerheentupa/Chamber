5e7f78642b2ef22b231c4ac7b478d988b68cd831
Karim Abdel Hamid <karimah@aucegypt.edu>
2020-05-17 11:15:19 +0200

    Fixed Post-Process for health not working

--- Chamber/Assets/Scripts/PostProcessManager.cs
+++ Chamber/Assets/Scripts/PostProcessManager.cs
@@ -17,45 +17,48 @@ public class PostProcessManager : MonoBehaviour {
     private float normalContrast;
 
     void Start() {
-        GameObject player = GameObject.Find("Player");
-        if (player) {
-            health = player.GetComponent<PlayerHealth>();
+        health = FindObjectOfType<PlayerHealth>();
+        if (health) {
+            ColorCurves colorSplines     = null;
+
+            Volume volume = gameObject.GetComponent<Volume>();
+            volume.profile.TryGet<ColorCurves>(out colorSplines);
+            volume.profile.TryGet<Vignette>(out vignette);
+
+            normalVignetteIntensity = vignette.intensity.value;
+            normalVignetteColor = vignette.color.value;
+
+            //normalContrast = contrast.value;
+
+            hueVsSat = colorSplines.hueVsSat;
+            hueVsSat.value.AddKey(0.00f, 0.5f);
+            hueVsSat.value.AddKey(0.08f, 0.5f);
+            hueVsSat.value.AddKey(0.92f, 0.5f);
         }
-        
-        ColorCurves colorSplines     = null;
-
-        Volume volume = gameObject.GetComponent<Volume>();
-        volume.profile.TryGet<ColorCurves>(out colorSplines);
-        volume.profile.TryGet<Vignette>(out vignette);
-
-        normalVignetteIntensity = vignette.intensity.value;
-        normalVignetteColor = vignette.color.value;
-
-        //normalContrast = contrast.value;
-
-        hueVsSat = colorSplines.hueVsSat;
-        hueVsSat.value.AddKey(0.00f, 0.5f);
-        hueVsSat.value.AddKey(0.08f, 0.5f);
-        hueVsSat.value.AddKey(0.92f, 0.5f);
+        else {
+            Debug.Log ("Post Processing: Can't get Player Health");
+        }
     }
 
     void Update() {
-        float hp = health.currentHealth / health.maximumHealth;
-        
-        ColorCurves colorSplines     = null;
-        
-        Volume volume = gameObject.GetComponent<Volume>();
-        volume.profile.TryGet<ColorCurves>(out colorSplines);
-        volume.profile.TryGet<Vignette>(out vignette);
+        if (health) {
+            float hp = health.currentHealth / health.maximumHealth;
+            
+            ColorCurves colorSplines     = null;
+            
+            Volume volume = gameObject.GetComponent<Volume>();
+            volume.profile.TryGet<ColorCurves>(out colorSplines);
+            volume.profile.TryGet<Vignette>(out vignette);
 
-        vignette.intensity.value = Mathf.Lerp(deadVignetteIntensity, normalVignetteIntensity, hp);
-        vignette.color.value = Color.Lerp(deadVignetteColor, normalVignetteColor, hp);
+            vignette.intensity.value = Mathf.Lerp(deadVignetteIntensity, normalVignetteIntensity, hp);
+            vignette.color.value = Color.Lerp(deadVignetteColor, normalVignetteColor, hp);
 
-        //contrast.value = Mathf.Lerp(deadContrast, normalContrast, hp);
+            //contrast.value = Mathf.Lerp(deadContrast, normalContrast, hp);
 
-        hueVsSat = colorSplines.hueVsSat;
-        hueVsSat.value.MoveKey(0, new Keyframe(0.00f, 1.0f - 0.4f * hp));
-        hueVsSat.value.MoveKey(1, new Keyframe(0.08f, 0.6f * hp));
-        hueVsSat.value.MoveKey(2, new Keyframe(0.92f, 0.6f * hp));
+            hueVsSat = colorSplines.hueVsSat;
+            hueVsSat.value.MoveKey(0, new Keyframe(0.00f, 1.0f - 0.4f * hp));
+            hueVsSat.value.MoveKey(1, new Keyframe(0.08f, 0.6f * hp));
+            hueVsSat.value.MoveKey(2, new Keyframe(0.92f, 0.6f * hp));
+        }
     }
 }
