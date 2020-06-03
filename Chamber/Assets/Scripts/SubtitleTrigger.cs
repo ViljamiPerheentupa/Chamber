21d5b24129458dc6294c858d9bdc989aa284ddbf
Karim Abdel Hamid <karimah@aucegypt.edu>
2020-06-04 00:37:28 +0200

    Fix VolumeTrigger, Add SubtitleTrigger

---
+++ Chamber/Assets/Scripts/SubtitleTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubtitleTrigger : MonoBehaviour {
+    public void TriggerSubtitle(string text) {
+        SubtitleManager sm = FindObjectOfType<SubtitleManager>();
+        if (sm) {
+            sm.AddSubtitle(text);
+        }
+    }
+}
--- Chamber/Assets/Scripts/VolumeTrigger.cs
+++ Chamber/Assets/Scripts/VolumeTrigger.cs
@@ -7,16 +7,25 @@ public class VolumeTrigger : MonoBehaviour {
     public LayerMask contactLayerMask = (1 << 9);
     public UnityEvent onEnter;
     public UnityEvent onExit;
+    
+    [Tooltip("If this is enabled, OnExit will only be called when the last item leaves the trigger, and OnEnter will only be called when the first item enters it.")]
+    public bool combineTriggers = true;
+
+    private int numItems = 0;
 
     private void OnTriggerEnter(Collider other) {
         if (contactLayerMask == (contactLayerMask | (1 << other.gameObject.layer))) {
-            onEnter.Invoke();
+            if (numItems++ == 0 || !combineTriggers) {
+                onEnter.Invoke();
+            }
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (contactLayerMask == (contactLayerMask | (1 << other.gameObject.layer))) {
-            onExit.Invoke();
+            if (--numItems == 0 || !combineTriggers) {
+                onExit.Invoke();
+            }
         }
     }
 }
