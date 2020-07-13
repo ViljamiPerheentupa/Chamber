7dfe5732b104743560876e2a12f371afbc9f87f2
Samuel <32210043+Satsaa@users.noreply.github.com>
2020-07-13 15:45:51 +0300

    Highlight created file

--- Chamber/Assets/Scripts/Texture2DArrayCreator.cs
+++ Chamber/Assets/Scripts/Texture2DArrayCreator.cs
@@ -67,6 +67,7 @@ namespace Hidden {
           }
 
           if (textureArray) AssetDatabase.CreateAsset(textureArray, path);
+          EditorGUIUtility.PingObject(textureArray);
         }
 
       }
